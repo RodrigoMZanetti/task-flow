0a3f48be3dc01a50809300397ef8c1d8d9ae9305
Rodrigo Maturano Zanetti <rzanetti.dev@outlook.com>
2026-08-18 13:32:01 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import "./App.css";
+
+//components
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <h1>Content</h1>
+      <Footer />
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <footer>
+      <span>Task Flow</span> @ 2026
+    </footer>
+  );
+}
+
+export default Footer;
